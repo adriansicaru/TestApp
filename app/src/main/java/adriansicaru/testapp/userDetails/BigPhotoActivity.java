a189bcb52a11ddde2470f6a2d54398aebd7962aa
adriansicaru <adrian.sicaru@infoworld.ro>
2017-09-17 23:46:51 +0300

    - pinch zoom detection

---
+++ app/src/main/java/adriansicaru/testapp/userDetails/BigPhotoActivity.java
@@ -0,0 +1,12 @@
+package adriansicaru.testapp.userDetails;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adriansicaru on 9/17/17.
+ */
+
+public class BigPhotoActivity extends AppCompatActivity {
+
+
+}
--- app/src/main/java/adriansicaru/testapp/userDetails/UserDetailsActivity.java
+++ app/src/main/java/adriansicaru/testapp/userDetails/UserDetailsActivity.java
@@ -2,14 +2,21 @@ package adriansicaru.testapp.userDetails;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonElement;
 import com.squareup.picasso.Picasso;
@@ -34,6 +41,13 @@ public class UserDetailsActivity extends AppCompatActivity {
     TextView nameTextView;
     TextView idText, addressText, emailText, phoneText;
     ImageView headerIcon;
+    CardView addressCard, phoneCard, emailCard;
+    RelativeLayout headerContainer;
+    int touchState;
+    final int IDLE = 0;
+    final int TOUCH = 1;
+    final int PINCH = 2;
+    float dist0, distCurrent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +73,11 @@ public class UserDetailsActivity extends AppCompatActivity {
         emailText = (TextView) findViewById(R.id.email_content);
         phoneText = (TextView) findViewById(R.id.phone_content);
 
+        addressCard = (CardView) findViewById(R.id.address_card);
+        phoneCard = (CardView) findViewById(R.id.phone_card);
+        emailCard = (CardView) findViewById(R.id.email_card);
+        headerContainer = (RelativeLayout) findViewById(R.id.header_container);
+
         addressText.setText(result.getLocation().getCity()+ " " + result.getLocation().getPostcode()
                 + " " + result.getLocation().getState()+ result.getLocation().getStreet());
         emailText.setText(result.getEmail());
@@ -70,6 +89,99 @@ public class UserDetailsActivity extends AppCompatActivity {
                 .transform(new CircleTransformation()).placeholder(R.drawable.user_placeholder).into(headerIcon);
 
 
+        addressCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=" +Uri.encode(result.getLocation().getState()+"+"
+                        +result.getLocation().getCity()+"+"+result.getLocation().getStreet()+"+"
+                        +result.getLocation().getPostcode()));
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    Toast.makeText(mContext, "No navigation app available", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        phoneCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "tel:" + result.getPhone() ;
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
+        emailCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uriText =
+                        "mailto:" + result.getEmail();
+
+                Uri uri = Uri.parse(uriText);
+
+                Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+                sendIntent.setData(uri);
+                startActivity(sendIntent);
+            }
+        });
+
+
+
+        headerContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // TODO Auto-generated method stub
+                float distx, disty;
+                switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        touchState = TOUCH;
+                        break;
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        touchState = PINCH;
+                        distx = motionEvent.getX(0) - motionEvent.getX(1);
+                        disty = motionEvent.getY(0) - motionEvent.getY(1);
+                        dist0 = (float) Math.sqrt(distx * distx + disty * disty);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+
+                        if (touchState == PINCH) {
+                            distx = motionEvent.getX(0) - motionEvent.getX(1);
+                            disty = motionEvent.getY(0) - motionEvent.getY(1);
+                            distCurrent = (float) Math.sqrt(distx * distx + disty * disty);
+                            logZoom();
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        touchState = IDLE;
+                        float curScale = distCurrent / dist0;
+                        if (curScale > 1) {
+                            Intent intent = new Intent(mContext, BigPhotoActivity.class);
+                            startActivity(intent);
+                        };
+                        resetZoom();
+                        break;
+                    case MotionEvent.ACTION_POINTER_UP:
+                        touchState = TOUCH;
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void logZoom() {
+        float curScale = distCurrent / dist0;
+        Log.e("ZOOM",curScale+"");
+    }
+    private void resetZoom() {
+        distCurrent = 1;
+        dist0 = 1;
+        logZoom();
+        touchState = IDLE;
     }
 
 }
